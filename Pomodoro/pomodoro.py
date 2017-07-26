69852bb2b96d8b4a4b66bd46a007bf3541536428
Auralcat <ashleyzt@hi2.in>
2017-07-25 22:41:58 -0300

    Implemented MVC in pomodoro.py

--- Pomodoro/pomodoro.py
+++ Pomodoro/pomodoro.py
@@ -4,45 +4,18 @@
 import time
 import sys
 
-class Pomodoro():
+class PomodoroModel():
     """The Pomodoro object holds the data about the completed pomodoros and the
        methods to interact with it."""
 
     total_pomodoros = 0
 
-    def __init__(self, display_func, duration=25):
+    def __init__(self, duration=25):
         """Sets the duration of a pomodoro session and initializes
            the variables"""
         self.duration = duration
         self.mins = 0
         self.secs = 0
-        self.display_func = display_func()
-
-        self.start()
-        self.stop()
-
-    def start(self, msg="Beginning pomodoro... focus!"):
-        """Begins counting the time."""
-        print(msg)
-
-        while self.mins != self.duration:
-            self.display_func()
-            self.secs += 1
-            if self.secs == 60:
-                self.mins += 1
-                self.secs = 0
-            time.sleep(1)
-
-    def stop(self, msg="Pomodoro finished!"):
-        """Adds a complete pomodoro to the count and opens
-           the pomocount file."""
-        print(msg)
-        self.total_pomodoros += 1
-        self.mins = 0
-        self.secs = 0
-
-    def interrupt(self):
-        """Stops the pomodoro and returns to original state"""
 
     def get_time(self):
         """Returns a string with the elapsed pomodoro time."""
@@ -51,12 +24,51 @@ class Pomodoro():
 
         return display_time
 
-def display(reading):
-    """External function to display the elapsed time"""
-    sys.stdout.write(reading + '\r')
-    sys.stdout.flush()
+class PomodoroController():
+    """Controls the model."""
+
+    def __init__(self, model):
+        self.model = model
+
+    def start(self, msg="Beginning pomodoro... focus!"):
+        """Begins counting the time."""
+        print(msg)
+        view = PomodoroView(self.model)
+        while self.model.mins != self.model.duration:
+            view.time()
+            self.model.secs += 1
+            if self.model.secs == 60:
+                self.model.mins += 1
+                self.model.secs = 0
+            time.sleep(1)
+        self.stop()
+
+    def stop(self, msg="Pomodoro finished!"):
+        """Adds a complete pomodoro to the count and opens
+           the pomocount file."""
+        print(msg)
+        self.model.total_pomodoros += 1
+        self.model.mins = 0
+        self.model.secs = 0
+
+    def interrupt(self):
+        """Stops the pomodoro and returns to original state"""
+
+
+class PomodoroView():
+    """Holds the methods to view the data inside PomodoroModel instances."""
+
+    def __init__(self, model):
+        self.model = model
+
+    def time(self):
+        sys.stdout.write(self.model.get_time() + '\r')
+        sys.stdout.flush()
 
 def test():
-    p = Pomodoro(1, display)
+    p = PomodoroModel(1)
+    c = PomodoroController(p)
+    c.start()
 
 test()
+
