29a8b5155b9ece82e359d17ddf445bfad2f8256b
Auralcat <ashleyzt@hi2.in>
2017-08-11 00:35:17 -0300

    More cleanup

--- Pomodoro/core/__init__.py
+++ Pomodoro/core/__init__.py
@@ -12,10 +12,16 @@ Basic usage:
     controller = pomocontroller.PomodoroController(model)
     controller.lifecycle()
 
+What makes everything work in here is the lifecycle() method. That's where the
+pomodoros will run. When one is finished, a break will start, and then it
+moves on to the next pomodoro until the long break.
+
 TODO:
 
-    - Make a place for a generic print function to be added"""
-
+    - Make a place for a generic print function to be added
+    - Implement unit tests
+    - Make the usage less verbose
+"""
 
 __title__ = 'pomodoro'
 __version__ = '0.5'
